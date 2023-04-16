0bdba94bc54c1ef8c4f22ff382735191eba67eaa
佐野貴史 <sanotakashi@MacBook-Air.local>
2023-04-16 18:10:03 +0900

    package.json script

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Notion Next.js blog</title>
+        <title>Tack's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -47,9 +47,9 @@ export default function Home({ posts }) {
               </g>
             </svg>
           </div>
-          <h1>Next.js + Notion API ブログ</h1>
+          <h1>たっくさんBlog</h1>
           <p>
-           Notionと連携しているブログです。Notionに書き込めばそのままブログとして投稿できます。
+           つれづれなるままにひぐらし、、、ICTについて
           </p>
         </header>
 
@@ -87,5 +87,15 @@ export default function Home({ posts }) {
   );
 }
 
-//SSGを追加
+//ISRを追加
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
 
+  return{
+    props: {
+      posts: database,
+    },
+    revalidate: 60,
+  };
+
+};
